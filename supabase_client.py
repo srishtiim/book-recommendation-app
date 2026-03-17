a7896701cec59b3e9ace4287e2f29805a32fa137
srishtiim <133870723+srishtiim@users.noreply.github.com>
2026-03-17 21:49:57 +0530

    remove old streamlit files

--- supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-import streamlit as st
-
-def get_supabase():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_ANON_KEY"]
-    return create_client(url, key)
